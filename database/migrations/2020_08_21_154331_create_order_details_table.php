1857ebebaee843a9bc84242ccca3ff4541f400d3
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-08-22 01:32:17 +0800

    first commit, schema database

---
+++ database/migrations/2020_08_21_154331_create_order_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id'); // field ini akan merujuk ke table orders
+            $table->unsignedBigInteger('product_id'); // field ini akan merujuk ke table products
+            $table->integer('price'); // ini sama dengan customer, informasi harga saat barang ini dipesan juga dibuat salinannya
+            $table->integer('qty');
+            $table->integer('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
