2e5dfd02eb5169221d09e3dfe47a50167eb4dfb5
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-08-22 02:40:30 +0800

    add templating and authentication

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
         $this->call(ProvinceTableSeeder::class);
         $this->call(CityTableSeeder::class);
         $this->call(DistrictTableSeeder::class);
+        $this->call(UsersTableSeeder::class); //TAMBAHKAN CODE INI
     }
 }
